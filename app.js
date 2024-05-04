ef8706fedbba6d6d105545af00dd33b4e35e4b4c
jai03sah <jaiprakashsah57@gmail.com>
2024-05-04 08:58:11 +0530

    second commit

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ const expressSession = require("express-session");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const passport = require('passport');
-
-var app = express();
+const { app } = require('./app.1');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
